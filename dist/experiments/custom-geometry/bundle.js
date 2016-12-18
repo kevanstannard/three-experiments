3c9b77fb5c659091c91d7f2ba0f1badbb2faec59
Kevan Stannard <kevanstannard@gmail.com>
2016-12-18 22:13:50 +1100

    Build

---
+++ dist/experiments/custom-geometry/bundle.js
@@ -0,0 +1,162 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	var SCREEN_WIDTH = window.innerWidth;
+	var SCREEN_HEIGHT = window.innerHeight;
+	var VIEW_ANGLE = 45;
+	var ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT;
+	var NEAR = 1;
+	var FAR = 10000;
+
+	var scene = void 0;
+	var camera = void 0;
+	var renderer = void 0;
+	var axisHelper = void 0;
+	var gridHelper = void 0;
+	var orbitControls = void 0;
+	var pointLight = void 0;
+	var ambientLight = void 0;
+	var mesh = void 0;
+	var controls = void 0;
+	var stats = void 0;
+
+	var origin = new THREE.Vector3(0, 0, 0);
+
+	function initStats() {
+	  stats = new Stats();
+	  stats.domElement.style.position = 'absolute';
+	  stats.domElement.style.left = '0px';
+	  stats.domElement.style.top = '20px';
+	  stats.setMode(0); // 0: fps, 1: ms
+	  document.getElementById('stats').appendChild(stats.domElement);
+	}
+
+	function initControls() {
+	  controls = {
+	    xRotation: 0,
+	    yRotation: 0,
+	    zRotation: 0
+	  };
+	  var gui = new dat.GUI();
+	  gui.add(controls, 'xRotation', 0, Math.PI * 2);
+	  gui.add(controls, 'yRotation', 0, Math.PI * 2);
+	  gui.add(controls, 'zRotation', 0, Math.PI * 2);
+	}
+
+	function init() {
+	  scene = new THREE.Scene();
+
+	  gridHelper = new THREE.GridHelper(100, 10);
+	  scene.add(gridHelper);
+
+	  axisHelper = new THREE.AxisHelper(100);
+	  scene.add(axisHelper);
+
+	  var vertices = [new THREE.Vector3(30, 0, 0), new THREE.Vector3(0, 0, 30), new THREE.Vector3(-30, 0, 0), new THREE.Vector3(0, 0, -30), new THREE.Vector3(0, 30, 0)];
+
+	  // Anti-clockwise = outer face
+	  // Clockwise = inner face
+	  var faces = [new THREE.Face3(4, 1, 0), new THREE.Face3(4, 2, 1), new THREE.Face3(4, 3, 2), new THREE.Face3(4, 0, 3), new THREE.Face3(1, 2, 3), new THREE.Face3(3, 0, 1)];
+
+	  var geometry = new THREE.Geometry();
+	  geometry.vertices = vertices;
+	  geometry.faces = faces;
+	  geometry.computeFaceNormals();
+
+	  var material = new THREE.MeshLambertMaterial({
+	    opacity: 0.5,
+	    color: 0x44ff44,
+	    transparent: true,
+	    side: THREE.DoubleSide
+	  });
+
+	  mesh = new THREE.Mesh(geometry, material);
+	  mesh.position.set(0, 20, 0);
+	  scene.add(mesh);
+
+	  ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+	  scene.add(ambientLight);
+
+	  pointLight = new THREE.PointLight(0xffffff, 1, 1000);
+	  pointLight.position.set(50, 200, -100);
+	  scene.add(pointLight);
+
+	  camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+	  camera.position.set(100, 100, 100);
+	  camera.lookAt(origin);
+
+	  renderer = new THREE.WebGLRenderer({ antialias: true });
+	  renderer.setSize(window.innerWidth, window.innerHeight);
+
+	  orbitControls = new THREE.OrbitControls(camera, renderer.domElement);
+
+	  THREEx.WindowResize(renderer, camera);
+
+	  document.body.appendChild(renderer.domElement);
+
+	  initStats();
+	  initControls();
+	}
+
+	function update() {
+	  mesh.rotation.set(mesh.rotation.x = controls.xRotation, mesh.rotation.y = controls.yRotation, mesh.rotation.z = controls.zRotation);
+	  stats.update();
+	  orbitControls.update();
+	}
+
+	function animate() {
+	  requestAnimationFrame(animate);
+	  update();
+	  renderer.render(scene, camera);
+	}
+
+	init();
+	animate();
+
+/***/ }
+/******/ ]);
---
+++ dist/experiments/multiple-materials/bundle.js
@@ -0,0 +1,160 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+	var SCREEN_WIDTH = window.innerWidth;
+	var SCREEN_HEIGHT = window.innerHeight;
+	var VIEW_ANGLE = 45;
+	var ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT;
+	var NEAR = 1;
+	var FAR = 10000;
+
+	var scene = void 0;
+	var camera = void 0;
+	var renderer = void 0;
+	var axisHelper = void 0;
+	var gridHelper = void 0;
+	var orbitControls = void 0;
+	var pointLight = void 0;
+	var ambientLight = void 0;
+	var mesh = void 0;
+	var controls = void 0;
+	var stats = void 0;
+
+	var origin = new THREE.Vector3(0, 0, 0);
+
+	function initStats() {
+	  stats = new Stats();
+	  stats.domElement.style.position = 'absolute';
+	  stats.domElement.style.left = '0px';
+	  stats.domElement.style.top = '20px';
+	  stats.setMode(0); // 0: fps, 1: ms
+	  document.getElementById('stats').appendChild(stats.domElement);
+	}
+
+	function initControls() {
+	  controls = {
+	    xRotation: 0,
+	    yRotation: 0,
+	    zRotation: 0
+	  };
+	  var gui = new dat.GUI();
+	  gui.add(controls, 'xRotation', 0, Math.PI * 2);
+	  gui.add(controls, 'yRotation', 0, Math.PI * 2);
+	  gui.add(controls, 'zRotation', 0, Math.PI * 2);
+	}
+
+	function init() {
+	  scene = new THREE.Scene();
+
+	  gridHelper = new THREE.GridHelper(100, 10);
+	  scene.add(gridHelper);
+
+	  axisHelper = new THREE.AxisHelper(100);
+	  scene.add(axisHelper);
+
+	  var baseOptions = {
+	    transparent: true,
+	    opacity: 0.8,
+	    side: THREE.DoubleSide
+	  };
+
+	  var material = new THREE.MultiMaterial([new THREE.MeshBasicMaterial(_extends({}, baseOptions, { color: 0xff0000 })), new THREE.MeshBasicMaterial(_extends({}, baseOptions, { color: 0x00ff00 })), new THREE.MeshBasicMaterial(_extends({}, baseOptions, { color: 0x0000ff }))]);
+
+	  var geometry = new THREE.BoxGeometry(50, 50, 50);
+
+	  // face.materialIndex is an index into the MultiMaterial above
+	  geometry.faces.forEach(function (face, index) {
+	    Object.assign(face, { materialIndex: index % 3 });
+	  });
+
+	  mesh = new THREE.Mesh(geometry, material);
+	  scene.add(mesh);
+
+	  ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+	  scene.add(ambientLight);
+
+	  pointLight = new THREE.PointLight(0xffffff, 1, 1000);
+	  pointLight.position.set(50, 200, -100);
+	  scene.add(pointLight);
+
+	  camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+	  camera.position.set(200, 200, 200);
+	  camera.lookAt(origin);
+
+	  renderer = new THREE.WebGLRenderer({ antialias: true });
+	  renderer.setSize(window.innerWidth, window.innerHeight);
+
+	  orbitControls = new THREE.OrbitControls(camera, renderer.domElement);
+
+	  THREEx.WindowResize(renderer, camera);
+
+	  document.body.appendChild(renderer.domElement);
+
+	  initStats();
+	  initControls();
+	}
+
+	function update() {
+	  mesh.rotation.set(mesh.rotation.x = controls.xRotation, mesh.rotation.y = controls.yRotation, mesh.rotation.z = controls.zRotation);
+	  stats.update();
+	  orbitControls.update();
+	}
+
+	function animate() {
+	  requestAnimationFrame(animate);
+	  update();
+	  renderer.render(scene, camera);
+	}
+
+	init();
+	animate();
+
+/***/ }
+/******/ ]);
