64cfec5448758c4981c077396085fa2440df01a9
Kevan Stannard <kevanstannard@gmail.com>
2016-12-20 06:31:14 +1100

    Update stats.js

--- src/experiments/hello-world/config.js
+++ src/experiments/hello-world/config.js
@@ -6,7 +6,7 @@ export default {
   scripts: [
     '../../libs/three/r83/three.min.js',
     '../../libs/three/r83/controls/OrbitControls.js',
-    '../../libs/three/r83/utils/stats.min.js',
+    '../../libs/stats/r17/stats.min.js',
     '../../libs/threex/THREEx.WindowResize.js',
     '../../libs/dat.gui/0.6.2/dat.gui.min.js',
   ],
