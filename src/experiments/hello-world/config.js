4521af70070abb153cac636c6b0be1bd15d26973
Kevan Stannard <kevanstannard@gmail.com>
2017-01-31 18:39:07 +1100

    Tweak hello world

--- src/experiments/hello-world/config.js
+++ src/experiments/hello-world/config.js
@@ -5,6 +5,7 @@ export default {
   public: true,
   scripts: [
     '../../libs/three/r83/three.min.js',
+    // 'http://127.0.0.1:8080/build/three.js',
     '../../libs/three/r83/controls/OrbitControls.js',
     '../../libs/stats/r17/stats.min.js',
     '../../libs/threex/THREEx.WindowResize.js',
