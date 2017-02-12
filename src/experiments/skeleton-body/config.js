4e881451aeb333368b2513dc60d7daf0d78a153d
Kevan Stannard <kevanstannard@gmail.com>
2017-02-12 17:11:01 +1100

    Add skeleton body experiment

---
+++ src/experiments/skeleton-body/config.js
@@ -0,0 +1,16 @@
+export default {
+  title: 'Skeleton Body',
+  description: 'Simple Skeleton body with multiple parts experiment',
+  tags: 'Skeleton, Bone, Mesh',
+  public: false,
+  scripts: [
+    '../../libs/three/r84/three.js',
+    '../../libs/three/r84/controls/OrbitControls.js',
+    '../../libs/stats/r17/stats.min.js',
+    '../../libs/threex/THREEx.WindowResize.js',
+    '../../libs/dat.gui/0.6.2/dat.gui.min.js',
+  ],
+  styles: [
+    '../../libs/dat.gui/0.6.2/dat.gui.css',
+  ],
+};
