0468923ddacdcbdaea79dd0eca73b2846c63533a
Kevan Stannard <kevanstannard@gmail.com>
2017-02-13 18:57:12 +1100

    Use minified three for skeleton-body

--- src/experiments/skeleton-body/config.js
+++ src/experiments/skeleton-body/config.js
@@ -2,9 +2,9 @@ export default {
   title: 'Skeleton Body',
   description: 'Simple Skeleton body with multiple parts experiment',
   tags: 'Skeleton, Bone, Mesh',
-  public: false,
+  public: true,
   scripts: [
-    '../../libs/three/r84/three.js',
+    '../../libs/three/r84/three.min.js',
     '../../libs/three/r84/controls/OrbitControls.js',
     '../../libs/stats/r17/stats.min.js',
     '../../libs/threex/THREEx.WindowResize.js',
