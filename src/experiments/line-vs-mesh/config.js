9151bcdb4fb394ba064935cb2079af2dec24e8a5
Kevan Stannard <kevanstannard@gmail.com>
2017-02-19 10:40:51 +1100

    Add Line vs Mesh experiment

---
+++ src/experiments/line-vs-mesh/config.js
@@ -0,0 +1,17 @@
+export default {
+  title: 'Line vs Mesh',
+  description: 'Explore difference between a Line and a Mesh',
+  tags: 'Line, Mesh',
+  public: true,
+  scripts: [
+    '../../libs/three/r84/three.min.js',
+    // 'http://127.0.0.1:8080/build/three.js',
+    '../../libs/three/r84/controls/OrbitControls.js',
+    '../../libs/stats/r17/stats.min.js',
+    '../../libs/threex/THREEx.WindowResize.js',
+    '../../libs/dat.gui/0.6.2/dat.gui.min.js',
+  ],
+  styles: [
+    '../../libs/dat.gui/0.6.2/dat.gui.css',
+  ],
+};
