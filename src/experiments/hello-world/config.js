32e1ec5ce328aaa405c30714eecbe83812b8657a
Kevan Stannard <kevanstannard@gmail.com>
2018-04-22 08:32:31 +1000

    Update hello world to use r92

--- src/experiments/hello-world/config.js
+++ src/experiments/hello-world/config.js
@@ -5,8 +5,8 @@ export default {
   public: false,
   scripts: [
     // 'http://127.0.0.1:8080/build/three.js', // For three.js dev
-    '../../libs/three/r87/three.min.js',
-    '../../libs/three/r87/controls/OrbitControls.js',
+    '../../libs/three/r92/three.min.js',
+    '../../libs/three/r92/controls/OrbitControls.js',
     '../../libs/threex/THREEx.WindowResize.js',
     '../../libs/dat.gui/0.6.2/dat.gui.min.js',
   ],
